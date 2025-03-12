dee911cf3c0a62d9ae939568f1c1f757f71a26fd
DevNadimm <nadimchowdhury87@gmail.com>
2025-03-12 22:50:34 +0600

    Add initial implementation of Hello World and student information display

---
+++ variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var studentId int = 123942 
+	var studentName string = "Nadim Chowdhury"
+	var studentMajor string = "CSE"
+	var studentAge int = 21
+
+	fmt.Println("\n")
+	fmt.Println("Student ID:", studentId)
+	fmt.Println("Student Name:", studentName)
+	fmt.Println("Student Major:", studentMajor)
+	fmt.Println("Student Age:", studentAge)
+
+
+	// Alternative
+	sId := 200384
+	sName := "Nadim"
+
+
+	fmt.Printf("\n\n")
+	fmt.Printf("Student ID: %d\n", sId)
+	fmt.Printf("Student Name: %s\n", sName)
+	
+	// Variable update
+	sName = "Md. Nadimul Haque Chowdhury"
+	fmt.Printf("\n\n")
+	fmt.Println("Unpdated Name:", sName)
+	fmt.Printf("\n\n")
+}
